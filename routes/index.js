b437e6c59d988a4194c3d2c968f12026ea3de682
Anwesha <iamanwesha17@gmail.com>
2023-01-23 14:12:56 +0530

    Implement CRUD operations on user

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
+const authRouter = require('./auth');
 const userRouter = require('./user');
 
+
 router.get('/', (req, res)=>{
-    res.send("route checked");
+    res.json("main route checked");
 })
 
+router.use('/auth', authRouter);
 router.use('/user', userRouter);
+
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,66 +1,84 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const userRepo = require('../models/User.js');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-router.get('/', (req, res)=>{
-    res.send("user route checked");
-})
 
-//registration logic
-router.post('/register', async (req, res)=>{
-    console.log(req.body);
-    try {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        const user = new userRepo({
-            username : req.body.name,
-            email : req.body.email,
-            password : hashedPassword,
-        })
-        console.log(user);
-        await user.save();
-        res.status(201).json({message: "user saved succesfully"});
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({error: error.message});
-
-    }
-})
-
-//login logic
-router.post('/login', async(req, res) => {
-    const user = await userRepo.findOne({email: req.body.email});
-    if (user) {
-        //if user exists, match password
-        const match = await bcrypt.compare(req.body.password, user.password);
-        if (match) {
-            //password matched, create jwt token
-            const token = jwt.sign(
-                {   user_id: user._id,
-                    email: user.email
-                },
-                process.env.TOKEN_KEY,
-                {
-                  expiresIn: "8h",
-                }
-              );
-        
-            // save user token
-            user.token.push(token);
-            console.log(user);
-
-            const {_id: userId, username, email} = user;
-
-            res.json({ userId, username, email, token});
-            return;
+router.get('/me', async(req, res)=>{
+    try{
+        const tokenUser = jwt.verify(req.headers['x-token'], process.env.TOKEN_KEY);
+        const userId = tokenUser.user_id;
+        try {
+            const user = await userRepo.findOne({_id : userId}, {__v:0, token:0, password:0});
+            res.json(user);
+        } catch(err) {
+            res.json(err.message);
         }
-        // else {
-        //     //wrong password
-        //     res.status(401).json({message: "incorrect credentials"});
-        // }
+    } catch(err) {
+        res.json({message: "Invalid token"});
     }
-    res.status(401).json({message : "incorrect credentials"});
-})
+
+    res.json();
+});
+router.get('/:id', async(req, res)=>{
+    userId = req.params.id;
+    try {
+        const user = await userRepo.findOne({_id : userId}, {
+            username: 1,
+            profilePicture: 1,
+            coverPicture: 1,
+            followers: 1,
+            following: 1
+        })
+        res.json(user);
+    } catch(err) {
+        res.json({message: err.message});
+    }
+});
+router.get('/', async(req, res)=>{
+    const name = req.query.name;
+    try {
+        const user = await userRepo.find({ username : { $regex : name, $options : 'i' } }, {
+            username: 1,
+            profilePicture: 1,
+            coverPicture: 1,
+            followers: 1,
+            following: 1
+        })
+        res.json(user);
+    } catch(err) {
+        res.json({message: err.message});
+    }
+});
+
+router.put('/me', async(req, res) => {
+    try{
+        const tokenUser = jwt.verify(req.headers['x-token'], process.env.TOKEN_KEY);
+        const userId = tokenUser.user_id;
+        try {
+            const updatedUser = await userRepo.findOneAndUpdate({_id : userId},{$set: req.body}, { new: true});
+            res.json(updatedUser);
+        } catch(err) {
+            res.json(err.message);
+        }
+    } catch(err) {
+        res.json({message: "Invalid token"});
+    }   
+});
+
+router.delete('/me', async(req, res) => {
+    try{
+        const tokenUser = jwt.verify(req.headers['x-token'], process.env.TOKEN_KEY);
+        const userId = tokenUser.user_id;
+        try {
+            await userRepo.deleteOne({_id: userId});
+            res.status(204).json();
+        } catch(err) {
+            res.json(err.message);
+        }
+    } catch(err) {
+        res.json({message: "Invalid token"});
+    }   
+});
 
 module.exports = router;
